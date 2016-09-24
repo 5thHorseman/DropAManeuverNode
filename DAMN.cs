27dfb65e956072b775314b79c119f7ec5aaa1374
HMV Plays <5thHorseman@users.noreply.github.com>
2016-09-23 23:05:46 -0400

    Add files via upload

--- DAMN.cs
+++ DAMN.cs
@@ -19,6 +19,7 @@ namespace DropAManeuverNode
     {
         ApplicationLauncherButton ToolbarButton;
         private bool ShowGUI = false;
+        private bool ShowedGUI = false;
 
         public void Awake()
         {
@@ -46,13 +47,44 @@ namespace DropAManeuverNode
         {
             if (ToolbarButton == null)
             {
-                ToolbarButton = ApplicationLauncher.Instance.AddModApplication(ToggleGUI, ToggleGUI, null, null, null, null, ApplicationLauncher.AppScenes.MAPVIEW | ApplicationLauncher.AppScenes.TRACKSTATION, GameDatabase.Instance.GetTexture("DropAManeuverNode/DAMN_Toolbar", false));
+                ToolbarButton = ApplicationLauncher.Instance.AddModApplication(ToggleGUI, ToggleGUI, null, null, null, null, ApplicationLauncher.AppScenes.MAPVIEW, GameDatabase.Instance.GetTexture("DropAManeuverNode/DAMN_Toolbar", false));
             }
+        }
+
+        public double timeToTargetAN(Orbit orbit)
+        {
+            Orbit orbitT = FlightGlobals.fetch.VesselTarget.GetOrbit();
+            Vector3d nodeAsc = Vector3d.Cross(orbitT.GetOrbitNormal(), orbit.GetOrbitNormal());
+            double anomalyAsc = orbit.GetTrueAnomalyOfZupVector(nodeAsc) * Mathf.Rad2Deg;
+            double time = orbit.GetDTforTrueAnomaly(anomalyAsc * Mathf.Deg2Rad, orbit.period);
+            return time < 0.0 ? time + orbit.period : time;
+        }
+
+        public double timeToTargetDN(Orbit orbit)
+        {
+            Orbit orbitT = FlightGlobals.fetch.VesselTarget.GetOrbit();
+            Vector3d nodeAsc = Vector3d.Cross(orbit.GetOrbitNormal(), orbitT.GetOrbitNormal());
+            double anomalyAsc = orbit.GetTrueAnomalyOfZupVector(nodeAsc) * Mathf.Rad2Deg;
+            double time = orbit.GetDTforTrueAnomaly(anomalyAsc * Mathf.Deg2Rad, orbit.period);
+            return time < 0.0 ? time + orbit.period : time;
+        }
+
+        public double timeToAN(Orbit orbit)
+        {
+            double time = orbit.GetDTforTrueAnomaly((360.0 - orbit.argumentOfPeriapsis) * UnityEngine.Mathf.Deg2Rad, orbit.period);
+            return time < 0.0 ? time + orbit.period : time;
+        }
+
+        public double timeToDN(Orbit orbit)
+        {
+            double time = orbit.GetDTforTrueAnomaly((180.0 - orbit.argumentOfPeriapsis) * UnityEngine.Mathf.Deg2Rad, orbit.period);
+            return time < 0.0 ? time + orbit.period : time;
         }
 
         public void ToggleGUI()
         {
             ShowGUI = !ShowGUI;
+            if (!ShowGUI) ShowedGUI = false;
         }
 
         private void OnGUI()
@@ -60,22 +92,56 @@ namespace DropAManeuverNode
             if (ShowGUI)
             {
                 MainMenu = GUI.Window(0, MainMenu, DrawMainMenu, "Drop A Maneuver Node");
+                if (!ShowedGUI)
+                {
+                    MainMenu.x = Mouse.screenPos.x - 210;
+                    MainMenu.y = Mouse.screenPos.y + 5;
+                    ShowedGUI = true;
+                }
             }
         }
 
-        public Rect MainMenu = new Rect(200, 200, 205, 235);
+        public Rect MainMenu = new Rect(200, 200, 210, 20 + (9 * 25) + 5);
+
         void DrawMainMenu(int windowID)
         {
             ManeuverNode node;
             double time;
-            int vpos = 20;
+            int vpos = 0;
+            int hpos = 5;
             GUI.DragWindow(new Rect(0, 0, 10000, 20));
 
-            if (GUI.Button(new Rect(5, vpos, 200, 20), "5 Minutes From Now"))
+            // ======================================================= TIME ===============================================
+            GUI.Label(new Rect(hpos, vpos += 25, 200, 20), "Time:");
+            if (GUI.Button(new Rect(hpos, vpos += 25, 35, 20), "1m"))
+            {
+                DropAManeuverNode_Time(60);
+                ShowGUI = false;
+            }
+            if (GUI.Button(new Rect(hpos += 40, vpos, 40, 20), "5m"))
             {
                 DropAManeuverNode_Time(5 * 60);
+                ShowGUI = false;
             }
-            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "At Periapsis"))
+            if (GUI.Button(new Rect(hpos += 40, vpos, 40, 20), "10m"))
+            {
+                DropAManeuverNode_Time(10 * 60);
+                ShowGUI = false;
+            }
+            if (GUI.Button(new Rect(hpos += 40, vpos, 40, 20), "1h"))
+            {
+                DropAManeuverNode_Time(60 * 60);
+                ShowGUI = false;
+            }
+            if (GUI.Button(new Rect(hpos += 40, vpos, 40, 20), "1d"))
+            {
+                DropAManeuverNode_Time(6 * 60 * 60);
+                ShowGUI = false;
+            }
+            hpos = 5;
+            // ======================================================= ABSOLUTE ===============================================
+            GUI.Label(new Rect(hpos, vpos += 25, 200, 20), "Orbit:");
+            if (GUI.Button(new Rect(hpos, vpos += 25, 45, 20), "Pe"))
             {
                 time = FlightGlobals.ActiveVessel.orbit.timeToPe;
                 if (time != 0)
@@ -86,7 +152,7 @@ namespace DropAManeuverNode
                     ShowGUI = false;
                 }
             }
-            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "At Apoapsis"))
+            if (GUI.Button(new Rect(hpos += 50, vpos, 45, 20), "Ap"))
             {
                 time = FlightGlobals.ActiveVessel.orbit.timeToAp;
                 if (time != 0)
@@ -95,16 +161,35 @@ namespace DropAManeuverNode
                     ShowGUI = false;
                 }
             }
-            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "Halfway Around Orbit"))
+            if (GUI.Button(new Rect(hpos += 50, vpos, 45, 20), "AN"))
             {
-                time = FlightGlobals.ActiveVessel.orbit.period/2;
+                time = timeToAN(FlightGlobals.ActiveVessel.orbit);
                 if (time != 0)
                 {
                     node = DropAManeuverNode_Time(time);
                     ShowGUI = false;
                 }
             }
-            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "Next Oribt"))
+            if (GUI.Button(new Rect(hpos += 50, vpos, 45, 20), "DN"))
+            {
+                time = timeToDN(FlightGlobals.ActiveVessel.orbit);
+                if (time != 0)
+                {
+                    node = DropAManeuverNode_Time(time);
+                    ShowGUI = false;
+                }
+            }
+            hpos = 5;
+            if (GUI.Button(new Rect(hpos, vpos += 25, 95, 20), "1/2 Orbit"))
+            {
+                time = FlightGlobals.ActiveVessel.orbit.period / 2;
+                if (time != 0)
+                {
+                    node = DropAManeuverNode_Time(time);
+                    ShowGUI = false;
+                }
+            }
+            if (GUI.Button(new Rect(hpos += 100, vpos, 95, 20), "1 Orbit"))
             {
                 time = FlightGlobals.ActiveVessel.orbit.period;
                 if (time != 0)
@@ -113,81 +198,106 @@ namespace DropAManeuverNode
                     ShowGUI = false;
                 }
             }
-            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "Next Closest Approach"))
+            hpos = 5;
+            // ======================================================= TARGET ===============================================
+            GUI.Label(new Rect(hpos, vpos += 25, 200, 20), "Target:");
+            if (GUI.Button(new Rect(hpos, vpos += 25, 95, 20), "AN"))
             {
-                time = FlightGlobals.ActiveVessel.orbit.closestTgtApprUT;
-                {
-                    print("DAMN: closest approach time = " + time);
-                    node = DropAManeuverNode_UT(time);
-                    ShowGUI = false;
-                }
-            }
-            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "timeToTransition1 (?)"))
-            {
-                time = FlightGlobals.ActiveVessel.orbit.timeToTransition1;
-                print("DAMN: Transition1 Time = " + time);
+                time = timeToTargetAN(FlightGlobals.ActiveVessel.orbit);
                 if (time != 0)
                 {
                     node = DropAManeuverNode_Time(time);
                     ShowGUI = false;
                 }
             }
-/*            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "DEBUG"))
+            if (GUI.Button(new Rect(hpos += 100, vpos, 95, 20), "DN"))
             {
-                print("DAMN: START DEBUG HERE!!!!!!!!!");
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.GetTimeToPeriapsis());
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.altitude);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.argumentOfPeriapsis);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.closestTgtApprUT);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.eccentricAnomaly);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.eccentricity);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.EndUT);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.epoch);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.FEVp);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.fromE);
-                print("DAMN: ------------------------------");
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.inclination);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.LAN);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.mag);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.meanAnomaly);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.meanAnomalyAtEpoch);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.meanMotion);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ObT);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ObTAtEpoch);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.orbitalEnergy);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.orbitalSpeed);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.orbitPercent);
-                print("DAMN: ------------------------------");
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.period);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.radius);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.semiMajorAxis);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.StartUT);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToAp);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToPe);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToTransition1);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.trueAnomaly);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ClAppr);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ClEctr1);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ClEctr2);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.CrAppr);
-                print("DAMN: ------------------------------");
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.E);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.FEVs);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.fromV);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.nearestTT);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.nextTT);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.sampleInterval);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.SEVp);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.SEVs);
-                print("DAMN: ------------------------------");
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToTransition2);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.toE);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.toV);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.UTappr);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.UTsoi);
-                print("DAMN: " + FlightGlobals.ActiveVessel.orbit.V);
-                print("DAMN: END DEBUG HERE!!!!!!!!!");
-            } */
+                time = timeToTargetDN(FlightGlobals.ActiveVessel.orbit);
+                if (time != 0)
+                {
+                    node = DropAManeuverNode_Time(time);
+                    ShowGUI = false;
+                }
+            }
+            MainMenu.height = vpos + 25;
+
+            //The below don't work so I'm removing them until I figure out why.
+            /*            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "Next Closest Approach"))
+                        {
+                            time = FlightGlobals.ActiveVessel.orbit.closestTgtApprUT;
+                            {
+                                print("DAMN: closest approach time = " + time);
+                                node = DropAManeuverNode_UT(time);
+                                ShowGUI = false;
+                            }
+                        }
+                        if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "timeToTransition1 (?)"))
+                        {
+                            time = FlightGlobals.ActiveVessel.orbit.timeToTransition1;
+                            print("DAMN: Transition1 Time = " + time);
+                            if (time != 0)
+                            {
+                                node = DropAManeuverNode_Time(time);
+                                ShowGUI = false;
+                            }
+                        }
+            */
+            /*            if (GUI.Button(new Rect(5, vpos += 25, 200, 20), "DEBUG"))
+                        {
+                            print("DAMN: START DEBUG HERE!!!!!!!!!");
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.GetTimeToPeriapsis());
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.altitude);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.argumentOfPeriapsis);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.closestTgtApprUT);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.eccentricAnomaly);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.eccentricity);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.EndUT);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.epoch);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.FEVp);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.fromE);
+                            print("DAMN: ------------------------------");
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.inclination);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.LAN);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.mag);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.meanAnomaly);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.meanAnomalyAtEpoch);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.meanMotion);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ObT);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ObTAtEpoch);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.orbitalEnergy);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.orbitalSpeed);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.orbitPercent);
+                            print("DAMN: ------------------------------");
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.period);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.radius);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.semiMajorAxis);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.StartUT);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToAp);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToPe);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToTransition1);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.trueAnomaly);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ClAppr);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ClEctr1);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.ClEctr2);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.CrAppr);
+                            print("DAMN: ------------------------------");
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.E);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.FEVs);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.fromV);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.nearestTT);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.nextTT);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.sampleInterval);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.SEVp);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.SEVs);
+                            print("DAMN: ------------------------------");
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.timeToTransition2);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.toE);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.toV);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.UTappr);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.UTsoi);
+                            print("DAMN: " + FlightGlobals.ActiveVessel.orbit.V);
+                            print("DAMN: END DEBUG HERE!!!!!!!!!");
+                        } */
         }
     }
 }
